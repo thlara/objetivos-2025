02660d049c3baec1e4fdf457dd949262699e8abf
thlara <105953614+thlara@users.noreply.github.com>
2024-04-11 20:46:10 -0300

    hh

--- main.js
+++ main.js
@@ -15,3 +15,8 @@ for (let i = 0; i < botoes.length; i++){
     }
     console.log(botoes);
 }
+
+
+const contadores = document.querySelectorAll( '.contador');
+const tempoObjetivo1 = new Date('2024-20-05');
+contadores[0]. textContent = tempoObjetivo1;

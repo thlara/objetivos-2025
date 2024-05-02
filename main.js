e2dd7b547e45ab49023a91a22f6bf6ff53b35c7f
thlara <105953614+thlara@users.noreply.github.com>
2024-05-02 20:17:27 -0300

    yt

--- main.js
+++ main.js
@@ -23,10 +23,11 @@ const tempoObjetivo2 = new Date('2025-03-01T00:00:00');
 const tempoObjetivo3 = new Date('2025-01-01T00:00:00');
 const tempoObjetivo4 = new Date('2025-01-01T00:00:00');
 
-contadores[0]. textContent = calculaTempo(tempoObjetivo1);
-contadores[1]. textContent = calculaTempo(tempoObjetivo2);
-contadores[2]. textContent = calculaTempo(tempoObjetivo3);
-contadores[3]. textContent = calculaTempo(tempoObjetivo4);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+for (let i = 0; i < contadores. length; i++) {
+    contadores[i].textContent = calculaTempo(tempos{i});
+}
 
 
 function calculaTempo(tempoObjetivo1) {

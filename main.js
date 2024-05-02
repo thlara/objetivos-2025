968c4fc8b0c761e4afafd264f14cc85e41a8d644
thlara <105953614+thlara@users.noreply.github.com>
2024-05-02 20:29:02 -0300

    eee

--- main.js
+++ main.js
@@ -27,13 +27,18 @@ const tempoObjetivo4 = new Date('2025-01-01T00:00:00');
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+function atualizaCronometro(){
+
 for (let i = 0; i < contadores. length; i++) {
     contadores[i].textContent = calculaTempo(tempos[i]);
 }
-
+}
+function comecaCronometro(){
 atualizaCronometro();
 setInterval(atualizaCronometro, 1000);
+}
 
+comecaCronometro();
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date ();

21c3a23a70592b3b8245a6f4a22631328dc1dd2e
thlara <105953614+thlara@users.noreply.github.com>
2024-05-02 20:18:20 -0300

    hh

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ const tempoObjetivo4 = new Date('2025-01-01T00:00:00');
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
 for (let i = 0; i < contadores. length; i++) {
-    contadores[i].textContent = calculaTempo(tempos{i});
+    contadores[i].textContent = calculaTempo(tempos[i]);
 }
 
 

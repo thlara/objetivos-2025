53af9ecd6658a69753870ad944d65b28d6a1119e
thlara <105953614+thlara@users.noreply.github.com>
2024-05-16 20:28:16 -0300

    t4t

--- main.js
+++ main.js
@@ -30,7 +30,14 @@ const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function atualizaCronometro(){
 
 for (let i = 0; i < contadores. length; i++) {
-    contadores[i].textContent = calculaTempo(tempos[i]);
+    document.getElementById('dias'+i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById('horas'+i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById('min'+i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById('seg'+i).textContent = calculaTempo(tempos[i])[3];
+
+    
+
+
 }
 }
 function comecaCronometro(){
@@ -38,7 +45,7 @@ atualizaCronometro();
 setInterval(atualizaCronometro, 1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date ();
@@ -53,9 +60,9 @@ function calculaTempo(tempoObjetivo1) {
     horas %=24;
 
     if (tempoFinal > 0){
-    return dias + 'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos' ;
+    return [dias, horas, minutos, segundos] ;
 }
 else {
-    return 'tempo Esgotado';
+    return [0,0,0,0];
 }
 }

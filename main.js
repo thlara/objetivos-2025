0a8b1fd6ba22a14cf35a33e6168aa9d2dbf70839
thlara <105953614+thlara@users.noreply.github.com>
2024-05-16 20:29:30 -0300

    ry

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ for (let i = 0; i < botoes.length; i++){
 
 
 const contadores = document.querySelectorAll('.contador');
-const tempoObjetivo1 = new Date('2023-02-13T00:00:00');
+const tempoObjetivo1 = new Date('2025-02-13T00:00:00');
 const tempoObjetivo2 = new Date('2025-03-01T00:00:00');
 const tempoObjetivo3 = new Date('2025-01-01T00:00:00');
 const tempoObjetivo4 = new Date('2025-01-01T00:00:00');
